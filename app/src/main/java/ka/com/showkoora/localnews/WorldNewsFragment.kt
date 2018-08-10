23b7975380a64cdac1d4b4c78be7313a4a0fe0f5
karim@karim-HP-ProBook-450-G1 <karimelhedaby@gmail.com>
2018-08-10 14:24:15 +0200

    //FRAGMENTS

--- app/src/main/java/ka/com/showkoora/localnews/WorldNewsFragment.kt
+++ app/src/main/java/ka/com/showkoora/localnews/WorldNewsFragment.kt
@@ -2,9 +2,12 @@ package ka.com.showkoora.localnews
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -14,28 +17,38 @@ import ka.com.showkoora.NewDetailsActivity
 import ka.com.showkoora.R
 import ka.com.showkoora.adapter.newsAdapter
 import kotlinx.android.synthetic.main.fragment_world_news.*
+import kotlinx.android.synthetic.main.fragment_world_news.view.*
 
-class WorldNewsFragment : AppCompatActivity(), newsAdapter.NewsClick {
+class WorldNewsFragment : Fragment(), newsAdapter.NewsClick {
+
+
+    companion object {
+        fun newInstance(): WorldNewsFragment {
+            var fragment = WorldNewsFragment()
+            return fragment
+        }
+    }
 
     override fun onNewsClickListener(new: New) {
-        var intent = Intent(this,NewDetailsActivity::class.java)
-        intent.putExtra("title",new.title)
-        intent.putExtra("image",new.image)
-        intent.putExtra("date",new.date)
-        intent.putExtra("desc",new.desc)
+        var intent: Intent = Intent(activity, NewDetailsActivity::class.java)
+        intent.putExtra("title", new.title)
+        intent.putExtra("image", new.image)
+        intent.putExtra("date", new.date)
+        intent.putExtra("desc", new.desc)
         startActivity(intent)
 
     }
 
-
     var news: ArrayList<New> = arrayListOf()
     lateinit var new: New
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_world_news)
 
-            worldnewsPB.visibility=View.VISIBLE
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layfragment
+        var view: View = inflater.inflate(R.layout.fragment_world_news, container, false)
+
+        view.worldnewsPB.visibility = View.VISIBLE
 
         FirebaseDatabase.getInstance().getReference("WorldNews").addValueEventListener(object : ValueEventListener {
 
@@ -44,27 +57,59 @@ class WorldNewsFragment : AppCompatActivity(), newsAdapter.NewsClick {
 
             override fun onDataChange(p0: DataSnapshot?) {
 
-                worldnewsPB.visibility=View.GONE
+                worldnewsPB.visibility = View.GONE
 
                 for (c in p0?.children!!) {
                     new = c.getValue(New::class.java)!!
                     news.add(new)
 
                 }
-                var reverseNews : ArrayList<List<New>> = arrayListOf(news.reversed())
+                var reverseNews: ArrayList<List<New>> = arrayListOf(news.reversed())
 
-
-
-                worldnewsRV.layoutManager = GridLayoutManager(this@WorldNewsFragment, 1)
-
-//                    worldnewsRV.layoutManager = LinearLayoutManager(this@WorldNewsFragment, LinearLayoutManager.VERTICAL,true)
-                worldnewsRV.adapter = this@WorldNewsFragment.let { newsAdapter(it, reverseNews[0], this@WorldNewsFragment) }
+                view.worldnewsRV.layoutManager = GridLayoutManager(context, 1)
+                view.worldnewsRV.adapter = this@WorldNewsFragment.let { newsAdapter(context!!.applicationContext, reverseNews[0], this@WorldNewsFragment) }
             }
 
         })
 
+        return view
     }
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_world_news)
+//
+//            worldnewsPB.visibility=View.VISIBLE
+//
+//        FirebaseDatabase.getInstance().getReference("WorldNews").addValueEventListener(object : ValueEventListener {
+//
+//            override fun onCancelled(p0: DatabaseError?) {
+//            }
+//
+//            override fun onDataChange(p0: DataSnapshot?) {
+//
+//                worldnewsPB.visibility=View.GONE
+//
+//                for (c in p0?.children!!) {
+//                    new = c.getValue(New::class.java)!!
+//                    news.add(new)
+//
+//                }
+//                var reverseNews : ArrayList<List<New>> = arrayListOf(news.reversed())
+//
+//
+//
+//                worldnewsRV.layoutManager = GridLayoutManager(this@WorldNewsFragment, 1)
+//
+//                    worldnewsRV.layoutManager = LinearLayoutManager(this@WorldNewsFragment, LinearLayoutManager.VERTICAL,false)
+////                    worldnewsRV.layoutManager = LinearLayoutManager(this@WorldNewsFragment, LinearLayoutManager.VERTICAL,true)
+//                worldnewsRV.adapter = this@WorldNewsFragment.let { newsAdapter(it, reverseNews[0], this@WorldNewsFragment) }
+//            }
+//
+//        })
 
 }
+
+
+
 
--- app/src/main/java/ka/com/showkoora/worldnews/Local_NewsFragment.kt
+++ app/src/main/java/ka/com/showkoora/worldnews/Local_NewsFragment.kt
@@ -2,9 +2,13 @@ package ka.com.showkoora.worldnews
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -14,52 +18,60 @@ import ka.com.showkoora.NewDetailsActivity
 import ka.com.showkoora.R
 import ka.com.showkoora.adapter.newsAdapter
 import kotlinx.android.synthetic.main.fragment_local__news.*
+import kotlinx.android.synthetic.main.fragment_local__news.view.*
 
-class Local_NewsFragment : AppCompatActivity(), newsAdapter.NewsClick {
+class Local_NewsFragment : Fragment(), newsAdapter.NewsClick {
 
-
-    var news: ArrayList<New> = arrayListOf()
-    lateinit var new: New
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_local__news)
-
-        localnewsPB.visibility = View.VISIBLE
-
-        FirebaseDatabase.getInstance().getReference("CityNews").addValueEventListener(object : ValueEventListener {
-
-            override fun onCancelled(p0: DatabaseError?) {
-            }
-
-            override fun onDataChange(p0: DataSnapshot?) {
-
-                localnewsPB.visibility = View.GONE
-
-                for (c in p0?.children!!) {
-                    new = c.getValue(New::class.java)!!
-                    news.add(new)
-
-                }
-
-                var reverseNews : ArrayList<List<New>> = arrayListOf(news.reversed())
-
-                localnewsRV.layoutManager = GridLayoutManager(this@Local_NewsFragment, 1)
-//                    localnewsRV.layoutManager = LinearLayoutManager(this@Local_NewsFragment, LinearLayoutManager.VERTICAL,true)
-                localnewsRV.adapter = this@Local_NewsFragment.let { newsAdapter(it, reverseNews[0], this@Local_NewsFragment) }
-
-            }
-        })
+    companion object {
+        fun newInstance(): Local_NewsFragment {
+            var fragment = Local_NewsFragment()
+            return fragment
+        }
     }
 
-
     override fun onNewsClickListener(new: New) {
-        var intent = Intent(this, NewDetailsActivity::class.java)
+        var intent: Intent = Intent(activity, NewDetailsActivity::class.java)
         intent.putExtra("title", new.title)
         intent.putExtra("image", new.image)
         intent.putExtra("date", new.date)
         intent.putExtra("desc", new.desc)
         startActivity(intent)
 
+    }
+
+    var news: ArrayList<New> = arrayListOf()
+    lateinit var new: New
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layfragment
+        var view: View = inflater.inflate(R.layout.fragment_world_news, container, false)
+
+        view.localnewsPB.visibility = View.VISIBLE
+
+        FirebaseDatabase.getInstance().getReference("WorldNews").addValueEventListener(object : ValueEventListener {
+
+            override fun onCancelled(p0: DatabaseError?) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot?) {
+
+               view.localnewsPB.visibility = View.GONE
+
+                for (c in p0?.children!!) {
+                    new = c.getValue(New::class.java)!!
+                    news.add(new)
+
+                }
+                var reverseNews: ArrayList<List<New>> = arrayListOf(news.reversed())
+
+                view.localnewsRV.layoutManager = GridLayoutManager(context, 1)
+                view.localnewsRV.adapter = this@Local_NewsFragment.let { newsAdapter(context!!.applicationContext, reverseNews[0], this@Local_NewsFragment) }
+            }
+
+        })
+
+        return view
     }
 }
